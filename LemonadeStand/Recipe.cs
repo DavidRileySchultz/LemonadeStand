3701e616dadfc7d2c1b370d4bdfc7b7999738123
DavidRileySchultz <rileyschultz@gmail.com>
2018-08-30 08:52:46 -0500

    finished giving the ability to change recipe, need to make a pitcher now.

--- LemonadeStand/Recipe.cs
+++ LemonadeStand/Recipe.cs
@@ -9,13 +9,22 @@ namespace LemonadeStand
     class Recipe
     {
         //member variables
+        public int cupsPerPitcher = 10;
         public int lemonsPerPitcher = 4;
         public int cupsOfSugarPerPitcher = 4;
-        public int iceCubesPerCup = 4;
-        
+        public int iceCubesPerPitcher = 40;
+
         //constructor
 
         //methods
+        public void DisplayCurrentRecipe()
+        {
+            Console.WriteLine("Your current recipe is:");
+            Console.WriteLine("Number of Lemons per pitcher = " + lemonsPerPitcher);
+            Console.WriteLine("Cups of Sugar per pitcher = " + cupsOfSugarPerPitcher);
+            Console.WriteLine("Number of Ice Cubes per pitcher = " + iceCubesPerPitcher);
+        }
+
         public void ChangeRecipe()
         {
             ChangeLemonAmount();
@@ -26,7 +35,7 @@ namespace LemonadeStand
 
         public void ChangeLemonAmount()
         {
-            Console.WriteLine("Many many Lemons do you want to use per pitcher?\n(1) 2 Lemons\n(2) 4 Lemons\n(3) 6 Lemons\n(4) Back to Change Recipe menu.");
+            Console.WriteLine("How many Lemons do you want to use per pitcher?\n(1) 2 Lemons\n(2) 4 Lemons\n(3) 6 Lemons\n(4) Exit back to Change Recipe menu.");
             switch(Console.ReadLine())
             {
                 case "1":
@@ -45,19 +54,56 @@ namespace LemonadeStand
                     Console.WriteLine("Please enter a valid input, try again...");
                     ChangeLemonAmount();
                     break;
-
             }
 
         }
 
         public void ChangeSugarAmount()
         {
-
+            Console.WriteLine("How many cups of sugar do you want to use per pitcher?\n(1) 2 Cups of Sugar\n(2) 4 Cups of Sugar\n(3) 6 Cups of Sugar\n(4) Exit back to Change Recipe menu." );
+            switch(Console.ReadLine())
+            {
+                case "1":
+                    cupsOfSugarPerPitcher = 2;
+                    break;
+                case "2":
+                    cupsOfSugarPerPitcher = 4;
+                    break;
+                case "3":
+                    cupsOfSugarPerPitcher = 6;
+                    break;
+                case "4":
+                    ChangeRecipe();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid input, try again...");
+                    ChangeSugarAmount();
+                    break;
+            }
         }
 
         public void ChangeIceAmount()
         {
-
+            Console.WriteLine("How many Ice Cubes do you want to use per pitcher?\n(1) 2 Ice Cubes per pitcher\n(2) 4 Ice Cubes per pitcher\n(3) 6 Ice Cubes per pitcher\n(4) Exit back to the Change Recipe menu.");
+            switch(Console.ReadLine())
+            {
+                case "1":
+                    iceCubesPerPitcher = 20;
+                    break;
+                case "2":
+                    iceCubesPerPitcher = 40;
+                    break;
+                case "3":
+                    iceCubesPerPitcher = 60;
+                    break;
+                case "4":
+                    ChangeRecipe();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid input, try again...");
+                    ChangeIceAmount();
+                    break;
+            }
         }
     }
 }
